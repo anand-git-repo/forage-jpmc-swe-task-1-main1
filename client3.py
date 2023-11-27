9b4c764b80f41eb0910b700b8593c356f332388a
Anand <singh.anand.cs@gmail.com>
2023-11-27 15:08:41 +0530

    Crete Patch file

--- client3.py
+++ client3.py
@@ -1,5 +1,4 @@
 ################################################################################
-#
 #  Permission is hereby granted, free of charge, to any person obtaining a
 #  copy of this software and associated documentation files (the "Software"),
 #  to deal in the Software without restriction, including without limitation
@@ -35,14 +34,14 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None
+    return price_a / price_b
 
 
 # Main
@@ -52,8 +51,12 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        price_dict = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            price_dict[stock] = price
 
-        print("Ratio %s" % getRatio(price, price))
+        ratio = getRatio(price_dict['ABC'], price_dict['DEF'])
+        if ratio is not None:
+            print("Ratio %s" % ratio)
